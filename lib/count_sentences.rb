b8b3ce46c58fdcbb92f0f5b2c7b638d0e60f861d
Natalie Taktachev <ntaktach@gmail.com>
2019-07-23 16:15:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
+attr_reader :string
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array_of_sentences = self.split(/[.!?]+/)
+    array_of_sentences.length
   end
 end
