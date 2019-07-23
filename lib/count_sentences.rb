2847192737d15aa2040bc973029185bdb1211408
Natalie Taktachev <ntaktach@gmail.com>
2019-07-23 15:20:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,11 @@ require 'pry'
 class String
 
   def sentence?(line)
-  if line[-1] = "." 
+  if line[-1] = "."
     return true
   else
     return false
-  end    
+  end
   end
 
   def question?
